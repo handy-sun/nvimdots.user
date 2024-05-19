4a540807202219109b631c77a9e2ee0f9ad4cb1a
sooncheer <handy-sun@foxmail.com>
2024-05-19 23:03:12 +0800

    Some FileType/BufRead changed

--- event.lua
+++ event.lua
@@ -1,14 +1,14 @@
 local definitions = {
 	bufs = {
 		{ "BufWritePre", "COMMIT_EDITMSG", "setlocal noundofile" },
+		{ "BufRead,BufNew", "tmux*.conf", "setf tmux" },
 		{ "BufRead,BufNew", "*.conf", "setf config" },
 		{ "BufRead,BufNew", "*.log", "setf messages" },
-		{ "FileType", "python", "set tabstop=4 shiftwidth=4 expandtab" },
-		{ "FileType", "yaml", "set tabstop=2 shiftwidth=2 expandtab" },
-		{ "FileType", "make", "set noexpandtab tabstop=8 softtabstop=0" },
+		{ "FileType", "yaml", "set shiftwidth=2 expandtab" },
 		{ "FileType", "lua", "set noexpandtab tabstop=4 softtabstop=0" },
-		{ "FileType", "cmake", "setlocal commentstring=# %s" },
 		{ "FileType", "systemd", "setlocal commentstring=# %s" },
+		{ "FileType", "crontab", "setlocal nobackup nowritebackup" },
+		{ "FileType", "help", "wincmd L | vertical resize -10" },
 	},
 }
 
