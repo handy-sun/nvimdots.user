94399e91799e5b7d8133b63f346bfe6da2e52f3a
sooncheer <handy-sun@foxmail.com>
2024-06-26 19:53:09 +0800

    make gitsigns deprecate message donnot show

---
+++ configs/gitsigns.lua
@@ -0,0 +1,13 @@
+return {
+	signs =  function()
+		return {
+			add          = { text = "+" },
+			change       = { text = "┃", },
+			delete       = { text = "_", },
+			topdelete    = { text = "▔", },
+			changedelete = { text = "~", },
+			untracked    = { text = '┆' },
+		}
+	end
+}
+
--- plugins/tool.lua
+++ plugins/tool.lua
@@ -1,14 +1,5 @@
 local tool = {}
 
--- tool['ibhagwan/fzf-lua'] = {
-	-- lazy = true,
-	-- name = 'fzf',
-	-- dependencies = { 'nvim-tree/nvim-web-devicons' },
-	-- config = function()
-		-- require('fzf-lua').setup({})
-	-- end
--- }
-
 tool['junegunn/fzf'] = {
 	lazy = true,
 	ft = 'fzf',
@@ -17,45 +8,49 @@ tool['junegunn/fzf'] = {
 	end
 }
 
-tool['ranjithshegde/ccls.nvim'] = {
-	lazy = true,
-	event = { 'BufReadPost' },
-	config = function ()
-		require("ccls").setup {
-			-- defaults = {
-			-- 	win_config = {
-			-- 		-- Sidebar configuration
-			-- 		sidebar = {
-			-- 			size = 50,
-			-- 			position = "topleft",
-			-- 			split = "vnew",
-			-- 			width = 50,
-			-- 			height = 20,
-			-- 		},
-			-- 		-- floating window configuration. check :help nvim_open_win for options
-			-- 		float = {
-			-- 			style = "minimal",
-			-- 			relative = "cursor",
-			-- 			width = 50,
-			-- 			height = 20,
-			-- 			row = 0,
-			-- 			col = 0,
-			-- 			border = "rounded",
-			-- 		},
-			-- 	},
-			-- 	filetypes = {"c", "cpp", "cc", "hpp", "objc", "objcpp"},
-
-			-- 	-- Lsp is not setup by default to avoid overriding user's personal configurations.
-			-- 	-- Look ahead for instructions on using this plugin for ccls setup
-			-- 	lsp = {
-			-- 		codelens = {
-			-- 			enabled = false,
-			-- 			events = {"BufEnter", "BufWritePost"}
-			-- 		}
-			-- 	}
-			-- }
-		}
-	end
+tool["folke/which-key.nvim"] = {
+	lazy = false;
 }
+
+-- tool['ranjithshegde/ccls.nvim'] = {
+-- 	lazy = true,
+-- 	event = { 'BufReadPost' },
+-- 	config = function ()
+-- 		require("ccls").setup {
+-- 			defaults = {
+-- 				win_config = {
+-- 					-- Sidebar configuration
+-- 					sidebar = {
+-- 						size = 50,
+-- 						position = "topleft",
+-- 						split = "vnew",
+-- 						width = 50,
+-- 						height = 20,
+-- 					},
+-- 					-- floating window configuration. check :help nvim_open_win for options
+-- 					float = {
+-- 						style = "minimal",
+-- 						relative = "cursor",
+-- 						width = 50,
+-- 						height = 20,
+-- 						row = 0,
+-- 						col = 0,
+-- 						border = "rounded",
+-- 					},
+-- 				},
+-- 				filetypes = {"c", "cpp", "cc", "hpp", "objc", "objcpp"},
+
+-- 				-- Lsp is not setup by default to avoid overriding user's personal configurations.
+-- 				-- Look ahead for instructions on using this plugin for ccls setup
+-- 				lsp = {
+-- 					codelens = {
+-- 						enabled = false,
+-- 						events = {"BufEnter", "BufWritePost"}
+-- 					}
+-- 				}
+-- 			}
+-- 		}
+-- 	end
+-- }
 
 return tool
