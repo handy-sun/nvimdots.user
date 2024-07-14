f816a47064ea7d66b76e03fa4daa66a0e70e6c8a
sooncheer <handy-sun@foxmail.com>
2024-07-14 21:45:11 +0800

    nmap <leader>h SwitchSrcHeader

--- event.lua
+++ event.lua
@@ -6,10 +6,15 @@ local definitions = {
 	},
 	ft = {
 		{ "FileType", "yaml", "set shiftwidth=2 expandtab" },
-		{ "FileType", "lua", "set noexpandtab tabstop=4 softtabstop=0" },
+		-- { "FileType", "lua", "set noexpandtab tabstop=4 softtabstop=0" },
 		{ "FileType", "systemd", "setlocal commentstring=# %s" },
 		{ "FileType", "crontab", "setlocal nobackup nowritebackup" },
 		{ "FileType", "help", "wincmd L | vertical resize -10" },
+		{
+			"FileType",
+			"c,cpp",
+			"nnoremap <leader>h :ClangdSwitchSourceHeader<CR>",
+		},
 	},
 }
 
